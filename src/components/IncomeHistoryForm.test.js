2354ab4b0f812cf86662e0b1db6a99c0a1b4604e
Alessandro Mancini <dott.alessandro.mancini@gmail.com>
2021-06-16 12:57:49 +0200

    test(incomehistoryform): added missing unit test

---
+++ src/components/IncomeHistoryForm.test.js
@@ -0,0 +1,287 @@
+import React from 'react'
+
+import { render, waitFor, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import IncomeHistoryForm from './IncomeHistoryForm'
+import userEvent from '@testing-library/user-event'
+
+let setIncomeType
+let setDate
+
+beforeEach(() => {
+  setIncomeType = jest.fn()
+  setDate = jest.fn()
+})
+
+afterEach(() => {
+  setIncomeType.mockRestore()
+  setDate.mockRestore()
+})
+
+test('rendering and select from day to week', async () => {
+  let incomeType = 'day'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  userEvent.selectOptions(screen.getByLabelText(/Incasso per/), 'week')
+
+  await waitFor(() => {
+    expect(setIncomeType).toHaveBeenCalledWith('week')
+    expect(setDate).toHaveBeenCalledWith(expect.any(Date))
+  })
+})
+
+test('rendering and select from day to month', async () => {
+  let incomeType = 'day'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  userEvent.selectOptions(screen.getByLabelText(/Incasso per/), 'month')
+
+  await waitFor(() => {
+    expect(setIncomeType).toHaveBeenCalledWith('month')
+    expect(setDate).toHaveBeenCalledWith(expect.any(Date))
+  })
+})
+
+test('rendering and select from week to month', async () => {
+  let incomeType = 'week'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  userEvent.selectOptions(screen.getByLabelText(/Incasso per/), 'month')
+
+  await waitFor(() => {
+    expect(setIncomeType).toHaveBeenCalledWith('month')
+    expect(setDate).toHaveBeenCalledWith(expect.any(Date))
+  })
+})
+
+test('rendering and select from week to day', async () => {
+  let incomeType = 'week'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  userEvent.selectOptions(screen.getByLabelText(/Incasso per/), 'day')
+
+  await waitFor(() => {
+    expect(setIncomeType).toHaveBeenCalledWith('day')
+    expect(setDate).toHaveBeenCalledWith(expect.any(Date))
+  })
+})
+
+test('rendering and select from month to week', async () => {
+  let incomeType = 'month'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  userEvent.selectOptions(screen.getByLabelText(/Incasso per/), 'week')
+
+  await waitFor(() => {
+    expect(setIncomeType).toHaveBeenCalledWith('week')
+    expect(setDate).toHaveBeenCalledWith(expect.any(Date))
+  })
+})
+
+test('rendering and select from month to day', async () => {
+  let incomeType = 'month'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  userEvent.selectOptions(screen.getByLabelText(/Incasso per/), 'day')
+
+  await waitFor(() => {
+    expect(setIncomeType).toHaveBeenCalledWith('day')
+    expect(setDate).toHaveBeenCalledWith(expect.any(Date))
+  })
+})
+
+test('rendering and click next button when incomeType is day', async () => {
+  let incomeType = 'day'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  let nextDay = new Date(date)
+  nextDay.setDate(date.getDate() + 1)
+
+  userEvent.click(screen.getByText('Successivo'))
+
+  await waitFor(() => {
+    expect(setDate).toHaveBeenCalledWith(nextDay)
+  })
+})
+
+test('rendering and click next button when incomeType is week', async () => {
+  let incomeType = 'week'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  let nextDay = new Date(date)
+  nextDay.setDate(date.getDate() + 7)
+
+  userEvent.click(screen.getByText('Successivo'))
+
+  await waitFor(() => {
+    expect(setDate).toHaveBeenCalledWith(nextDay)
+  })
+})
+
+test('rendering and click next button when incomeType is month', async () => {
+  let incomeType = 'month'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  let nextDay = new Date(date)
+  nextDay.setDate(
+    date.getDate() + new Date(date.getYear(), date.getMonth() + 1, 0).getDate()
+  )
+
+  userEvent.click(screen.getByText('Successivo'))
+
+  await waitFor(() => {
+    expect(setDate).toHaveBeenCalledWith(nextDay)
+  })
+})
+
+test('rendering and click prev button when incomeType is day', async () => {
+  let incomeType = 'day'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  let prevDay = new Date(date)
+  prevDay.setDate(date.getDate() - 1)
+
+  userEvent.click(screen.getByText('Precedente'))
+
+  await waitFor(() => {
+    expect(setDate).toHaveBeenCalledWith(prevDay)
+  })
+})
+
+test('rendering and click prev button when incomeType is week', async () => {
+  let incomeType = 'week'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  let prevDay = new Date(date)
+  prevDay.setDate(date.getDate() - 7)
+
+  userEvent.click(screen.getByText('Precedente'))
+
+  await waitFor(() => {
+    expect(setDate).toHaveBeenCalledWith(prevDay)
+  })
+})
+
+test('rendering and click prev button when incomeType is month', async () => {
+  let incomeType = 'month'
+  let date = new Date()
+
+  render(
+    <IncomeHistoryForm
+      incomeType={incomeType}
+      setIncomeType={setIncomeType}
+      setDate={setDate}
+      date={date}
+    />
+  )
+
+  let prevDay = new Date(date)
+  prevDay.setDate(
+    date.getDate() - new Date(date.getYear(), date.getMonth() + 1, 0).getDate()
+  )
+
+  userEvent.click(screen.getByText('Precedente'))
+
+  await waitFor(() => {
+    expect(setDate).toHaveBeenCalledWith(prevDay)
+  })
+})
--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,21 +1,13 @@
 import { useState, useEffect, useRef } from 'react'
-import { Form, Row, Col, Button } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 
 import Diagram from '../components/Diagram'
+import IncomeHistoryForm from '../components/IncomeHistoryForm'
 
 const label = {
   day: 'Incasso del Giorno',
   week: 'Incasso della Settimana',
   month: 'Incasso del Mese'
-}
-
-const steps = (type, date) => {
-  const steps = {
-    day: 1,
-    week: 7,
-    month: new Date(date.getYear(), date.getMonth() + 1, 0).getDate()
-  }
-  return steps[type]
 }
 
 function Statistics() {
@@ -46,20 +38,6 @@ function Statistics() {
     getData()
   }, [incomeType, date])
 
-  const prevDay = () => {
-    const step = steps(incomeType, date)
-    let prevDay = new Date(date)
-    prevDay.setDate(date.getDate() - step)
-    setDate(prevDay)
-  }
-
-  const nextDay = () => {
-    const step = steps(incomeType, date)
-    let nextDay = new Date(date)
-    nextDay.setDate(date.getDate() + step)
-    setDate(nextDay)
-  }
-
   return (
     <div className="mt-4">
       <h1>
@@ -76,50 +54,16 @@ function Statistics() {
       <Row className="mb-3 mt-5">
         <Col md={{ span: 12 }}>
           <p>
-            <strong>{label[incomeType]}: </strong> &#8364; {income}{' '}
-            {date.toISOString()}
+            <strong>{label[incomeType]}: </strong> &#8364; {income}
           </p>
         </Col>
       </Row>
 
-      <Row>
-        <Col md={{ span: 4 }} xs={{ span: 2 }} className="align-self-center">
-          <Button className="mt-3 float-left" variant="dark" onClick={prevDay}>
-            <i className="fas fa-arrow-left "></i>{' '}
-            <span className="d-none d-sm-inline-block">&nbsp; Precedente</span>
-          </Button>
-        </Col>
-        <Col md={{ span: 4 }} xs={{ span: 8 }}>
-          <Form.Group controlId="page-size">
-            <Form.Label>
-              <b>Incasso per</b> :
-            </Form.Label>
-            <Form.Control
-              as="select"
-              custom
-              value={incomeType}
-              onChange={(e) => {
-                setIncomeType(e.target.value)
-              }}>
-              {[
-                { label: 'Giorno', value: 'day' },
-                { label: 'Settimana', value: 'week' },
-                { label: 'Mese', value: 'month' }
-              ].map((type) => (
-                <option key={type.label} value={type.value}>
-                  {type.label}
-                </option>
-              ))}
-            </Form.Control>
-          </Form.Group>
-        </Col>
-        <Col md={{ span: 4 }} xs={{ span: 2 }} className="align-self-center">
-          <Button className="mt-3 float-right" variant="dark" onClick={nextDay}>
-            <span className="d-none d-sm-inline-block">Successivo &nbsp;</span>
-            <i className="fas fa-arrow-right"></i>
-          </Button>
-        </Col>
-      </Row>
+      <IncomeHistoryForm
+        incomeType={incomeType}
+        setIncomeType={setIncomeType}
+        date={date}
+        setDate={setDate}></IncomeHistoryForm>
     </div>
   )
 }
